2edd77fb917b97113916faca643fedf5d6ab83e3
ThorKongstad <thorkongstadmadsn@hotmail.com>
2026-09-18 15:18:30 +0300

    called wrong param copy

--- RMC_tb_gpaw/calculator_prepare.py
+++ RMC_tb_gpaw/calculator_prepare.py
@@ -17,5 +17,5 @@ def calculation_setter(atoms, calc_params, dftd4_bool, txt=None):
         if 'txt' in calc_params.keys(): calc_params.pop('txt', None)
     else: calc_params_copy.update({'txt': txt})
     if dftd4_bool: calc = SumCalculator([DFTD4(method=calc_params['xc']), GPAW(**calc_params_copy)])
-    else: calc = GPAW(**calc_params)
+    else: calc = GPAW(**calc_params_copy)
     atoms.calc = calc
